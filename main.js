8427c2b771babe354841e587e80b4ebfddf8bc89
Emmanuel Santiago <biosinx@gmail.com>
2021-01-20 20:30:35 -0500

    Nuevo contenido

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 console.log("Hola mundo");
 // Comentario =D
 
+// String
+"Esto es un ejemplo de string"
+"Este es otro ejemplo de string"
+
+// Numeros
+1000
+100
+10
+1
+
+// Boolean
+true
+false
+
+//Listas
 ["ejemplo","de","lista"]
 
+//Objetos
 {
     nombre: Emmanuel
     apellido: santiago
